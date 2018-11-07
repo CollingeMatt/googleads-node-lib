68c57625e2f898b584c9d29b28b859ff797a30df
Daniel Wardin <info@danielwardin.com>
2018-11-07 15:40:05 +0000

    Include customer service

---
+++ services/customerService.js
@@ -0,0 +1,93 @@
+var
+  _ = require('lodash'),
+  async = require('async'),
+  soap = require('soap');
+
+var AdWordsService = require('./adWordsService');
+var types = require('../types/customer');
+
+function Service(options) {
+  var self = this;
+  var Selector = require('../types/selector').model;
+  AdWordsService.call(self, options);
+  self.Collection = types.collection;
+  self.Model = types.model;
+
+  // why the cm?
+  self.operatorKey = 'cm:operator';
+  self.mutateRemove = null;
+  self.mutateSet = null;
+
+  self.parseGetResponse = function(response) {
+    if (self.validateOnly) {
+      return {
+        entries: null,
+        links: null,
+        'Page.Type': null,
+        totalNumEntries: null
+
+      };
+    } else {
+      if (response.rval) {
+        return {
+          entries: new self.Collection(response.rval.entries),
+          links: new self.Collection(response.rval.links),
+          'Page.Type': response.rval['Page.Type'],
+          totalNumEntries: response.rval.totalNumEntries
+        };
+      } else {
+        return {};
+      }
+    }
+  };
+
+  self.parseMutateResponse = function(response) {
+    if (self.validateOnly) {
+      return {
+        value: null
+      };
+    } else {
+      if (response.rval) {
+        return {
+          value: new self.Collection(response.rval.value)
+        };
+      } else {
+        return {};
+      }
+    }
+  };
+
+  self.parseMutateLinkResponse = function(response) {
+    if (self.validateOnly) {
+      return {
+        links: null
+      };
+    } else {
+      if (response.rval) {
+        return {
+          links: new self.Collection(response.rval.links)
+        };
+      } else {
+        return {};
+      }
+    }
+  };
+
+  self.selectable = [
+      'CustomerId',
+      'CurrencyCode',
+      'DateTimeZone',
+      'DescriptiveName',
+      'CanManageClients',
+      'TestAccount'
+  ];
+
+  self.xmlns = 'https://adwords.google.com/api/adwords/mcm/' + self.version;
+  self.wsdlUrl = self.xmlns + '/CustomerService?wsdl';
+}
+
+Service.prototype = _.create(AdWordsService.prototype, {
+  'constructor': Service
+});
+
+module.exports = (Service);
